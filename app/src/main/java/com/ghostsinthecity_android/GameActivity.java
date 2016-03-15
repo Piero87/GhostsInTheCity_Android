8b2753f32f3b95bec4e5820302c09324c99eecc8
Pietro Nompleggio <pietro.nompleggio@gmail.com>
2016-03-15 10:11:12 +0100

    working...

--- app/src/main/java/com/ghostsinthecity_android/GameActivity.java
+++ app/src/main/java/com/ghostsinthecity_android/GameActivity.java
@@ -1,9 +1,11 @@
 package com.ghostsinthecity_android;
 
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.widget.TextView;
 
+import com.beyondar.android.world.GeoObject;
 import com.ghostsinthecity_android.models.Game;
 import android.content.Intent;
 import android.view.Window;
@@ -57,14 +59,59 @@ public class GameActivity extends FragmentActivity implements OnClickBeyondarObj
         currentGame  = (Game )mIntent.getParcelableExtra("game");
 
         waiting_label = (TextView) findViewById(R.id.waiting_players_label);
-        waiting_label.setText("Waiting " + (currentGame.getN_players() - currentGame.getPlayers().size())+" more players...");
+        waiting_label.setText("Waiting " + (currentGame.getN_players() - currentGame.getPlayers().size()) + " more players...");
         waiting_label.setVisibility(View.GONE);
+
+        Location l = SocketLocation.getInstance().getLastLocation();
+
+        world = new World(this);
+        world.setGeoPosition(l.getLatitude(), l.getLongitude());
+        world.setDefaultImage(R.drawable.beyondar_default_unknow_icon);
+
+        GeoObject go1 = new GeoObject(1l);
+        go1.setGeoPosition(44.139768, 12.243406);
+        go1.setImageResource(R.drawable.ghost);
+        go1.setName("Ingresso Via Sacchi");
+
+        GeoObject go2 = new GeoObject(2l);
+        go2.setGeoPosition(44.139716, 12.242636);
+        go2.setImageResource(R.drawable.ghost);
+        go2.setName("Cessi Piano Terra");
+
+        GeoObject go3 = new GeoObject(3l);
+        go3.setGeoPosition(44.139997, 12.242686);
+        go3.setImageResource(R.drawable.ghost);
+        go3.setName("Biblioteca");
+
+        GeoObject go4 = new GeoObject(4l);
+        go4.setGeoPosition(44.139841, 12.243162);
+        go4.setImageResource(R.drawable.ghost);
+        go4.setName("Segreteria");
+
+        GeoObject go5 = new GeoObject(5l);
+        go5.setGeoPosition(44.146015, 12.252614);
+        go5.setImageResource(R.drawable.ghost);
+        go5.setName("Segreteria");
+
+        world.addBeyondarObject(go1);
+        world.addBeyondarObject(go2);
+        world.addBeyondarObject(go3);
+        world.addBeyondarObject(go4);
+        world.addBeyondarObject(go5);
+
+
+        mBeyondarFragment.setWorld(world);
+        mBeyondarFragment.setMaxDistanceToRender(50);
+        mBeyondarFragment.setSensorDelay(SensorManager.SENSOR_DELAY_GAME);
+        mBeyondarFragment.showFPS(true);
     }
 
 
     @Override
     public void updateLocation(Location location) {
 
+        world.setGeoPosition(location.getLatitude(), location.getLongitude());
+        mBeyondarFragment.setWorld(world);
     }
 
     @Override
