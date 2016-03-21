1aa6ea8344978ba59034586826a4dab74091d78f
Pietro Nompleggio <pietro.nompleggio@gmail.com>
2016-03-21 17:46:47 +0100

    working...

--- app/src/main/java/com/ghostsinthecity_android/GameLobby.java
+++ app/src/main/java/com/ghostsinthecity_android/GameLobby.java
@@ -62,7 +62,7 @@ public class GameLobby extends FragmentActivity implements GameEvent {
 
         final Button create_game = (Button) findViewById(R.id.create_btn);
 
-        final TextView n_player_txt = (EditText) findViewById(R.id.n_player_text);
+        final TextView n_player_txt = (TextView) findViewById(R.id.n_player_text);
         n_player_txt.setText(Integer.toString(n_players));
         final Button sub_player_btn = (Button) findViewById(R.id.sub_players);
         sub_player_btn.setOnClickListener(new View.OnClickListener() {
@@ -82,7 +82,7 @@ public class GameLobby extends FragmentActivity implements GameEvent {
             }
         });
 
-        final TextView arena_meters_txt = (EditText) findViewById(R.id.arena_meters);
+        final TextView arena_meters_txt = (TextView) findViewById(R.id.arena_meters);
         arena_meters_txt.setText(Integer.toString(arena_meters));
         final Button sub_meters_btn = (Button) findViewById(R.id.sub_meters);
         sub_meters_btn.setOnClickListener(new View.OnClickListener() {
